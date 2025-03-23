bd2b591c810e24ed21acf1a2dc9c2008a412427a
Adeyinka Agbogun <118022444+heisade@users.noreply.github.com>
2025-03-23 21:51:19 +0100

    Update Basic Bank App.py
    Few tweaks to the bank app

--- Intermediate Projects/Basic Bank App.py
+++ Intermediate Projects/Basic Bank App.py
@@ -1,27 +1,31 @@
 class BankAccount:
-    bank_name = "SafeTrust Bank"
-
-    def set_account(self, account_holder, account_number, account_balance):
-        self.user = account_holder
+    
+    def __init__(self,  account_holder_name, account_number, account_balance=0):
+        self.account_holder_name = account_holder_name
         self.account_number = account_number
         self.balance = account_balance
 
-    def deposit(self, deposit):
-        self.balance += deposit
-
-
-    def withdraw(self, withdrawal):
-        if withdrawal > self.balance:
-            print("Insufficient funds!")
+    def deposit(self, deposit_amount):
+        print("-" *60)
+        if deposit_amount > 0:
+            self.balance += deposit_amount
+            print(f"${deposit_amount} was added to balance")
+            print(f"💰 New Balance: ${self.balance}\n")
         else:
-            self.balance -= withdrawal
-
-    def get_account(self):
-        return f"Bank: {account.bank_name}\nAccount Holder: {self.user}\nAccount Number: {self.account_number}\nBalance: ${self.balance}"
-
-account = BankAccount()
-
-account.set_account("John Doe", "12345678", 5000)
-account.deposit(500)
-account.withdraw(1000)
-print(account.get_account())
+            print("Deposit amount must be greater than $0")
+        return
+    
+    def withdraw(self, withdraw_amount):
+        print("-" *60)
+        if withdraw_amount > self.balance:
+            print("❌ Insufficient Funds!")
+        else:
+            self.balance -= withdraw_amount
+            print(f"✅ ${withdraw_amount} has been withdrawn successfully!")
+            print(f"💰 New Balance: ${self.balance}\n")
+        return
+    
+    def check_balance(self):
+        print("-" *60)
+        print(f"👤 Account Holder: {self.account_holder_name}")
+        print(f"💰 Your balance is ${self.balance}")
