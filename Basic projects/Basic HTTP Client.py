fc9d440ed5b757490eed4c00848058ed5cecf8c1
Adeyinka Agbogun <118022444+heisade@users.noreply.github.com>
2025-05-22 16:28:21 +0100

    Add files via upload
    A simple HTTP client using Python sockets.

---
+++ Basic projects/Basic HTTP Client.py
@@ -0,0 +1,20 @@
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+site = input('Enter site you want to visit: ')
+path = input('Enter page path (e.g home): ')
+
+if not path.startswith('/'):
+    path = '/' + path
+
+mysock.connect((site, 80))
+cmd = f'GET {path} HTTP/1.0\r\nHost: {site}\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()
